2b90be6ff886b8bf307d31305bedfaeafe72238f
Seby Amador <81187812+Wo0fle@users.noreply.github.com>
2022-11-27 14:23:45 -0500

    boy oh boy i cant wait to deal with this

---
+++ old stuff/playground.py
@@ -0,0 +1,13 @@
+""" The place to test the virtual Square-1! """
+
+from squan import Squan
+
+
+def main():
+    """Main function"""
+
+    square_one = Squan()
+
+
+if __name__ == "__main__":
+    main()
