f275904abfe0e434c9e871dd0dca6850faa043ff
Woofle <officiallywoofle@gmail.com>
2024-06-16 22:31:46 -0400

    docs and setup
    im just guessing here... probably close enough

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from virtual_sq1 import __version__
+
+setup(
+    name='virtual_sq1',
+    version=__version__,
+
+    url='https://github.com/Wo0fle/virtual-sq1',
+    author='Seby Amador',
+    
+    py_modules=['virtual_sq1'],
+)
